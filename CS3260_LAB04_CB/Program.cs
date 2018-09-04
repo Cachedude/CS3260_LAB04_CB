4fe5ffd87e11c1c1bb9aafe51fc61777164eb619
Caleb Baker <cachedude@msn.com>
2018-09-04 13:09:54 -0600

    Add project files.

---
+++ CS3260_LAB04_CB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS3260_LAB04_CB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
